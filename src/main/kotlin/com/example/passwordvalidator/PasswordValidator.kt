f52828f14cd74837840017296dfc5245123405cd
Dagaet <alojera15@gmail.com>
2024-04-15 09:36:54 +0100

    Test(Green): passed tests for PasswordValidator

--- src/main/kotlin/com/example/passwordvalidator/PasswordValidator.kt
+++ src/main/kotlin/com/example/passwordvalidator/PasswordValidator.kt
@@ -1,8 +1,28 @@
 package com.example.passwordvalidator
 
 class PasswordValidator {
-    fun isPasswordValid(s: String): Any {
-        return ""
+    fun isPasswordValid(password: String): Boolean {
+        return password.length >= 8 && hasLowerCase(password) && hasNumber(password) && hasUnderscore(password)
+                && hasUpperCase(password)
     }
 
+    private fun  hasNumber(password: String):Boolean{
+        val number = password.count{it.isDigit()}
+        return number >= 1
+    }
+    private fun hasUnderscore(password: String):Boolean{
+        val predicateUnderscore: (Char) -> Boolean = { it == '_' }
+        val underscore = password.find(predicateUnderscore).toString()
+        return underscore.length == 1
+    }
+    private fun hasLowerCase(password: String):Boolean{
+        val regex = """[a-z]""".toRegex()
+        val lowerCases = regex.containsMatchIn(password)
+        return lowerCases
+    }
+    private fun hasUpperCase(password: String): Boolean{
+        val regex = """[A-Z]""".toRegex()
+        val upperCases = regex.containsMatchIn(password)
+        return upperCases
+    }
 }
